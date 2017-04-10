496cdb15da43471e53b2b85c83833978162c180e
Noah Huber-Feely <nhuberfeely@gmail.com>
2017-04-10 07:04:00 -0500

    Add setup.py file

---
+++ setup.py
@@ -0,0 +1,7 @@
+setup(
+    name='clarabot',
+    version='0.1.0',
+    install_requires=[],  # Don't put anything here, just use requirements.txt
+    packages=['brain'],
+    package_dir={'brain': './brain'},
+)
