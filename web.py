ffb860f4e17da963e071b033470e78ef5d0f3bd4
Noah Huber-Feely <nhuberfeely@gmail.com>
2017-04-12 11:13:50 -0500

    Make server actually deliver proper response to app or other interface

--- web.py
+++ web.py
@@ -1,5 +1,7 @@
 import brain
 import os
+from flask import json
+from flask import jsonify
 from flask import Flask
 from flask import request
 
@@ -11,8 +13,23 @@ def main():
 
 @app.route("/converse", methods=['POST'])
 def parse_request():
-    starter = request.form['input']
-    return brain.get_response(starter)
+    text = json.dumps(request.json)
+    message = request.json["input"]
+    message = message.lower()
+    response = brain.get_response(message)
+    if not response == None:
+        return response
+    else:
+        return "Sorry! I'm still learning to understanding."
+    # Legacy Tests kept for future difficulties
+    '''
+    return 'JSON Message: ' + json.dumps(request.json)
+    return request.args['input']
+    starter = request.json
+    return json.dumps(starter)
+    message = starter['input']
+    return 'Hey: ' + brain.get_response(message)
+    '''
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 2525))
