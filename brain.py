37f9d996efbfa6bbfe365f5ac951a9639d5e0556
Noah Huber-Feely <nhuberfeely@gmail.com>
2017-05-13 11:04:59 -0400

    Add persistent feelings, and selective response

--- brain.py
+++ brain.py
@@ -2,9 +2,17 @@ from random import randint
 from Levenshtein import distance
 from os import listdir
 import json
+
+# Config load
 configFile = open('config.json')
 raw_data = configFile.read()
 data = json.loads(raw_data)
+
+# Emotion load
+emotionFile = open('emotions.json')
+raw_data = emotionFile.read()
+emotions = json.loads(raw_data)
+emotionFile.close()
 
 # Append all conversation response around distributed conversation files
 # This allows one to "plug-in" new responses and have them centralized together
@@ -15,6 +23,22 @@ for i in convoFiles:
         convoFile = open('convos/' + i)
         raw_data = convoFile.read()
         convo += json.loads(raw_data)
+
+# Var Setup
+VAR_REGISTRY = {}
+def build_registry():
+    global VAR_REGISTRY
+    VAR_REGISTRY = {
+            "user_name": data['user']['name'],
+            "name": data['name'],
+            "response_count": len(convo),
+            "user_hobby": data['user']['hobby'],
+            "happy_level": emotions['happy'],
+            "stress_level": emotions['stress'],
+            "animosity": emotions['animosity']
+            }
+
+build_registry()
 
 def punctuation_stripper(statement):
     toRemove = ['.', '!', '?']
@@ -35,10 +59,22 @@ def get_response(input):
         for a in i['starters']:
             val = distance(input, a)
             if len(input)/(val+1) > 1.5:
-                possibilities.append({'val': val, 'response': i['replies'][randint(0, len(i['replies']) - 1)]['text']})
+                reply_options = []
+                for b in i['replies']:
+                    try:
+                        to_test = b['qualifiers']
+                        for z in to_test:
+                            if VAR_REGISTRY[z['name']] == z['val']:
+                                reply_options += [b['text']]
+                            else:
+                                do_nothing = True
+                    except:
+                        reply_options += [b['text']]
+                slimmed_reply = reply_options[randint(0, len(reply_options)-1)]
+                possibilities.append({'val': val, 'response': slimmed_reply})
     min = 10000000000
     response = 'None'
-    print(possibilities)
+    # print(possibilities)
     for i in possibilities:
         if i['val'] < min:
             response = i['response']
@@ -47,7 +83,9 @@ def get_response(input):
             user_name=data['user']['name'],
             name=data['name'],
             response_count=len(convo),
-            user_hobby=data['user']['hobby']
+            user_hobby=data['user']['hobby'],
+            happy_level=emotions['happy'],
+            stress_level=emotions['stress']
                 )
 
 if __name__ == "__main__":
@@ -65,3 +103,6 @@ if __name__ == "__main__":
             logFile.write('R: ' + response + ender)
         else:
             logFile.write('R: None' + ender)
+    emotionFile = open('emotions.json', 'w')
+    emotionFile.write(json.dumps(emotions))
+    emotionFile.close()
