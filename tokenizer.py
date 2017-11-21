a57834d9a0b9aeb967d2b3f3cb55df0256118de6
Noah Huber-Feely <nhuberfeely@gmail.com>
2017-11-20 21:31:59 -0500

    Add full tokenizer and ML model building for matching input to response

--- tokenizer.py
+++ tokenizer.py
@@ -1,7 +1,3 @@
-'''''''
-Tokenizing for context inspired by https://chatbotsmagazine.com/contextual-chat-bots-with-tensorflow-4391749d0077
-'''''''
-
 from utils import convo_reader
 import json
 from os import listdir
@@ -28,7 +24,7 @@ with open('intents.json') as json_data:
 # Append all conversation response around distributed conversation files
 # This allows one to "plug-in" new responses and have them centralized together
 convo = []
-data = {'convo_dir': 'convos/'}
+data = {'convo_dir': '../convos/'}
 convoFiles = listdir(data['convo_dir'])
 for i in convoFiles:
     if i.endswith('.json'):
@@ -82,3 +78,61 @@ classes = sorted(list(set(classes)))
 print (len(documents), "documents")
 print (len(classes), "classes", classes)
 print (len(words), "unique stemmed words", words)
+
+#################################
+# Beggin Tensor flow operations #
+#################################
+
+# create our training data
+training = []
+output = []
+# create an empty array for our output
+output_empty = [0] * len(classes)
+
+# training set, bag of words for each sentence
+for doc in documents:
+    # initialize our bag of words
+    bag = []
+    # list of tokenized words for the pattern
+    pattern_words = doc[0]
+    # stem each word
+    pattern_words = [stemmer.stem(word.lower()) for word in pattern_words]
+    # create our bag of words array
+    for w in words:
+        bag.append(1) if w in pattern_words else bag.append(0)
+
+    # output is a '0' for each tag and '1' for current tag
+    output_row = list(output_empty)
+    output_row[classes.index(doc[1])] = 1
+
+    training.append([bag, output_row])
+
+# shuffle our features and turn into np.array
+random.shuffle(training)
+training = np.array(training)
+
+# create train and test lists
+train_x = list(training[:,0])
+train_y = list(training[:,1])
+print('Train x: ', train_x)
+print('Train y: ', train_y)
+
+# reset underlying graph data
+tf.reset_default_graph()
+# Build neural network
+net = tflearn.input_data(shape=[None, len(train_x[0])])
+net = tflearn.fully_connected(net, 8)
+net = tflearn.fully_connected(net, 8)
+net = tflearn.fully_connected(net, len(train_y[0]), activation='softmax')
+net = tflearn.regression(net)
+
+# Define model and setup tensorboard
+model = tflearn.DNN(net, tensorboard_dir='tflearn_logs')
+# Start training (apply gradient descent algorithm)
+epochs = 100
+model.fit(train_x, train_y, n_epoch=epochs, batch_size=8, show_metric=True)
+model.save('model.tflearn')
+
+# save all of our data structures
+import pickle
+pickle.dump( {'words':words, 'classes':classes, 'train_x':train_x, 'train_y':train_y}, open( "training_data", "wb" ) )
