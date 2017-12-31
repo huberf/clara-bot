a2e4e1c7e7f670a02945421dbf84bab656914e88
Noah Huber-Feely <nhuberfeely@gmail.com>
2017-12-31 15:26:07 -0500

    Add new feelings system and better IO

--- brain.py
+++ brain.py
@@ -5,6 +5,7 @@ from os import listdir
 import json
 from threading import Thread
 from time import sleep
+import re
 # Import interface for basic convo file
 from utils import convo_reader
 from message_statistics import MessageStats
@@ -25,15 +26,16 @@ emotionFile.close()
 # Append all conversation response around distributed conversation files
 # This allows one to "plug-in" new responses and have them centralized together
 convo = []
+convoDir = data['convo_dir']
 convoFiles = listdir(data['convo_dir'])
 for i in convoFiles:
     if i.endswith('.json'):
-        convoFile = open('convos/' + i)
+        convoFile = open(convoDir + i)
         raw_data = convoFile.read()
         convo += json.loads(raw_data)
     elif i.endswith('.convo'):
         # Process the loose file format
-        convoFile = open('convos/' + i)
+        convoFile = open(convoDir + i)
         raw_data = convoFile.read()
         convo += convo_reader.convert_to_json(raw_data)
 
@@ -51,6 +53,9 @@ def build_registry():
             "stress_level": emotions['stress'],
             "animosity": emotions['animosity']
             }
+    feelings = json.load(open('feelings.json'))
+    for i in feelings:
+        VAR_REGISTRY[i['name']] = i['val']
 
 build_registry()
 
@@ -65,7 +70,10 @@ def punctuation_stripper(statement):
 
 def handle_modifiers(modifiers):
     for i in modifiers:
-        VAR_REGISTRY[i['name']] += i['val']
+        try:
+            VAR_REGISTRY[i['name']] += i['val']
+        except:
+            doNothing = True
 
 def calc_qualifiers(qualifier):
     registryValue = VAR_REGISTRY[qualifier['name']]
@@ -154,17 +162,25 @@ def get_response(input):
                             to_add['weight'] = 1
                         reply_options += [to_add]
                 slimmed_reply = random_pick_weighted(reply_options)
-                handle_modifiers(slimmed_reply['modifiers'])
-                possibilities.append({'val': val, 'response': slimmed_reply['text'], 'image': slimmed_reply['image'], 'weight': slimmed_reply['weight']})
+                possibilities.append({
+                    'val': val,
+                    'response': slimmed_reply['text'],
+                    'image': slimmed_reply['image'],
+                    'weight': slimmed_reply['weight'],
+                    'modifiers': slimmed_reply['modifiers']
+                    })
     min = 10000000000
     response = 'None'
     image = 'None'
+    modifiers = []
     # print(possibilities)
     for i in possibilities:
         if i['val'] < min:
             response = i['response']
             image = i['image']
+            modifiers = i['modifiers']
             min = i['val']
+    handle_modifiers(modifiers)
     toReturn = {'message': response.format(**VAR_REGISTRY), 'image': image}
     return toReturn
 
@@ -176,15 +192,16 @@ def threaded_input():
 
 ticker = 0
 def event_check():
+    global ticker
     ticker += 1
 
 if __name__ == "__main__":
     logFile = open('log.txt', 'a')
     secureLogger = MessageStats("secure_log.json")
     secureLogger.load_log()
-    print("Booting...")
+    iograb.put("Booting...")
     ioThread = Thread(target = threaded_input)
-    print("{} online.".format(data['name']))
+    iograb.put("{} online.".format(data['name']))
     ioThread.start()
     terminated = False
     while not terminated:
@@ -193,7 +210,7 @@ if __name__ == "__main__":
             statement = input_queue[0]
             del input_queue[0]
             response = get_response(statement.lower())
-            print(response['message'])
+            iograb.put(response['message'])
             secureLogger.log_occurence(response['message'])
             ender = '\n'
             logFile.write('Q: ' + statement + ender)
--- utils/iograb.py
+++ utils/iograb.py
@@ -1,3 +1,6 @@
 def get():
     text = input("> ");
     return text
+
+def put(text):
+    print(text)
